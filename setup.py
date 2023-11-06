02c23f0ff5cdc05dfa210084e376d63a7629581a
Bastien Carreres <bastien.carreres@duke.edu>
2023-11-06 13:37:38 -0500

    create needed file for pip installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import re
+import os 
+
+version=re.findall(r"__version__ = \"(.*?)\"",
+            open(os.path.join("snanapytools", "__init__.py")).read())[0]
+
+if __name__ == "__main__":
+    setup(version=version)
---
+++ snanapytools/__init__.py
@@ -0,0 +1,6 @@
+from . import tools
+import os
+
+__snanapytools_dir_path__ = os.path.dirname(__file__)
+
+__version__ = "0.1"
