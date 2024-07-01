b75a69f223c9ae3055ee21c8ec9e7f34385f1457
bastiencarreres <carreres@cppm.in2p3.fr>
2024-07-01 13:15:31 -0700

    add selec pippin sim

--- snanapytools/tools.py
+++ snanapytools/tools.py
@@ -3,12 +3,17 @@ import time
 import numpy as np
 import geopandas as gpd
 import re
+import copy
 import os
+import shutil
+import gzip
 from multiprocessing import Pool
 from numba import njit, guvectorize
 from shapely import geometry as shp_geo
 from shapely import ops as shp_ops
+from astropy.io import fits, ascii
 from astropy.table import Table
+from pathlib import Path
 from . import utils as ut
 
 class SNANAsim:
@@ -316,7 +321,7 @@ def read_hostlib(file):
     return pd.read_table(file, skiprows=i, delimiter=' ')
 
 class SNANA_PDF:
-    def __init__(pdf_dic):
+    def __init__(self, pdf_dic):
         self.dic = pdf_dic
         self.keys = list(pdf_dic.keys())
     
@@ -329,14 +334,149 @@ class SNANA_PDF:
         f = open(file, 'r')
         lines = np.array(f.readlines())
         lines = lines[~ut.vstartswith(lines, '#')]
-        varline = lines[ia].replace('VARNAMES:', '').replace('\n', '').strip().split(' ')
-        dic = {k: [] for k in varline}
-        for l in lines[ia+1:ib]:
-            if l != '\n':
-                vals = l.replace('PDF:', '').replace('\n', '').strip().split(' ')
-                for k, v in zip(dic, vals):
-                    dic[k].append(float(v))
-        pdf_dic[varline[0]] = pd.DataFrame(dic) 
+        varlines = np.where(ut.vstartswith(lines, 'VARNAMES'))[0]
+        pdf_dic = {}
+        for i, ia in enumerate(varlines):
+            if i < len(varlines) - 1:
+                ib = varlines[i + 1]
+            else:
+                ib = len(lines)
+            varnames = lines[ia].replace('VARNAMES:', '').replace('\n', '').strip().split(' ')
+            dic = {k: [] for k in varnames}
+            vallines = lines[ia+1:ib]
+            vallines = vallines[ut.vstartswith(vallines, 'PDF:')]
+            for l in vallines:
+                if l != '\n':
+                    vals = l.replace('PDF:', '').replace('\n', '').strip().split(' ')
+                    for k, v in zip(dic, vals):
+                        dic[k].append(float(v))
+            pdf_dic[varnames[0]] = pd.DataFrame(dic) 
         return pdf_dic
     
     
+def apply_selec(selec_prob, head_df, phot_df):
+    # Main function
+    mask = np.zeros(len(head_df), dtype='bool')
+    photmask = np.zeros(len(phot_df), dtype='bool')
+    for i in range(len(head_df)):
+        sn_prop = head_df.iloc[i]
+        sn_phot = phot_df.iloc[sn_prop['PTROBS_MIN']-1:sn_prop['PTROBS_MAX']]
+        min_mag = sn_phot['SIM_MAGOBS'][(sn_phot['BAND'].map(lambda x: x.strip()) == selec_prob['band'])].min()
+        p = selec_prob['prob'](min_mag)
+
+        if np.random.random() < p:
+            mask[i] = True
+            photmask[sn_prop['PTROBS_MIN']-1:sn_prop['PTROBS_MAX']] = True
+    return mask, photmask
+
+def apply_selec_pippin_dir(pip_dir, out_dir, selec_prob, suffix='', exclude=[], only_include=[], compress=True, overwrite=False):
+    __PIPPIN_OUTPUT__ = os.getenv('PIPPIN_OUTPUT')
+    
+    print(f'$PIPPIN_OUTPUT = {__PIPPIN_OUTPUT__}\n')
+    
+    PIP_DIR = Path(__PIPPIN_OUTPUT__ + '/' + pip_dir)
+    SIM_DIR = PIP_DIR / '1_SIM'
+    FIT_DIR = PIP_DIR / '2_LCFIT'
+    
+    OUT_DIR = Path(out_dir + f'/{pip_dir}_{suffix}')
+    SIM_OUT_DIR = OUT_DIR / '1_SIM'
+    FIT_OUT_DIR = OUT_DIR / '2_LCFIT'
+
+    print(f'WRITE IN {OUT_DIR.stem}')
+    
+    for sd, fd in zip(sorted(SIM_DIR.glob('*')), sorted(FIT_DIR.glob('*'))):
+        if sd.name in exclude:
+            print(f'{sd.name} EXCLUDED\n')
+            continue
+        elif (len(only_include) > 0) and (sd.name not in only_include):
+            print(f'{sd.name} EXCLUDED\n')
+            continue
+        
+        print(f'PROCESSING {sd.name}\n')
+
+        head_files = sorted(sd.glob('*/*_HEAD*'))
+        phot_files = sorted(sd.glob('*/*_PHOT*'))
+        
+        fitfiles = sorted(fd.glob('output/*/*.FITRES*'))
+        
+        VERSION_SIM_OUT_DIR = Path(SIM_OUT_DIR / sd.name)
+        VERSION_FIT_OUT_DIR = Path(FIT_OUT_DIR / fd.name)
+        VERSION_SIM_OUT_DIR.mkdir(parents=True, exist_ok=True)
+        (VERSION_FIT_OUT_DIR / 'output').mkdir(parents=True, exist_ok=True)
+
+
+        # Copy Pippin needed files
+        # SIM
+        shutil.copy2(sd / 'config.yml', VERSION_SIM_OUT_DIR)
+        shutil.copy2(list(sd.glob('PIP*.input'))[0], VERSION_SIM_OUT_DIR)
+        shutil.copy2(list(sd.glob('PIP*.LOG'))[0], VERSION_SIM_OUT_DIR)
+        shutil.copytree(sd / 'LOGS', VERSION_SIM_OUT_DIR / 'LOGS', dirs_exist_ok=True)
+        # FIT
+        shutil.copy2(fd / 'config.yml', VERSION_FIT_OUT_DIR)
+        shutil.copy2(list(fd.glob('FIT_PIP*.nml'))[0], VERSION_FIT_OUT_DIR)
+        shutil.copy2(list(fd.glob('FIT_PIP*.LOG'))[0], VERSION_FIT_OUT_DIR)
+        shutil.copy2(list(fd.glob('output/ALL.DONE'))[0], VERSION_FIT_OUT_DIR / 'output/ALL.DONE')
+        shutil.copy2(list(fd.glob('output/MERGE.LOG'))[0], VERSION_FIT_OUT_DIR / 'output/MERGE.LOG')
+
+        SN_IDs = []
+        for hf, pf in zip(head_files, phot_files):
+            
+            # Define new files
+            new_head_dir = VERSION_SIM_OUT_DIR / hf.parent.name
+            new_phot_dir = VERSION_SIM_OUT_DIR / pf.parent.name 
+            new_head_dir.mkdir(parents=True, exist_ok=True)
+            new_phot_dir.mkdir(parents=True, exist_ok=True)
+            
+            new_head_file = new_head_dir / hf.stem.replace('HEAD',f'{suffix.upper()}' + '_HEAD')
+            new_phot_file = new_phot_dir / pf.stem.replace('PHOT',f'{suffix.upper()}' + '_PHOT')
+            
+            if compress:
+                new_head_file = new_head_file.with_suffix('.FITS.gz')
+                new_phot_file = new_phot_file.with_suffix('.FITS.gz')
+                
+            # Open files
+            header = fits.open(hf)
+            phot = fits.open(pf)
+            phot_df = Table(phot[1].data).to_pandas()
+            head_df = Table(header[1].data)[['SNID', 'PTROBS_MIN', 'PTROBS_MAX', 'NOBS', 'SIM_REDSHIFT_CMB']].to_pandas()
+            
+            # Apply selec
+            mask, photmask = apply_selec(selec_prob, head_df, phot_df)
+
+            # Copy phot and head fits files
+            new_phot = copy.deepcopy(phot)
+            new_head = copy.deepcopy(header)
+            
+            # Change header data + header
+            new_head[0].header.set('photfile', new_phot_file.name)
+            new_head[1].data = new_head[1].data[mask]
+            
+            # Change phot data + header
+            new_phot[1].data = new_phot[1].data[photmask]
+            new_phot[1].header.set('naxis2', len(new_phot[1].data))
+            new_phot[0].header.set('photfile', new_phot_file.name)
+            
+            # Write 
+            fits.HDUList(new_head).writeto(new_head_file, overwrite=overwrite)
+            fits.HDUList(new_phot).writeto(new_phot_file, overwrite=overwrite)
+            
+            SN_IDs.append(new_head[1].data['SNID'])
+            
+        SN_IDs = np.hstack(SN_IDs).astype('int')
+        
+        print(f'PROCESSING {fd.name}\n')
+        for fitresf in sorted(fd.glob('output/*/*.FITRES*')):
+            # Define new fit files
+            new_fit_dir = VERSION_FIT_OUT_DIR / fitresf.parents[1].name / fitresf.parents[0].name
+            new_fit_dir.mkdir(parents=True, exist_ok=True)
+            new_fit_file = new_fit_dir / fitresf.stem.replace('.FITRES',f'_{suffix.upper()}' + '.FITRES')
+
+            # Read data
+            fitres = Table.read(fitresf, format='ascii')
+            mask = np.isin(fitres['CID'], SN_IDs)
+            ascii.write(fitres[mask], output=new_fit_file, overwrite=overwrite)
+            if compress:
+                with open(new_fit_file, 'rb') as f_in:
+                    with gzip.open(new_fit_file.with_suffix('.FITRES.gz'),'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                new_fit_file.unlink()
